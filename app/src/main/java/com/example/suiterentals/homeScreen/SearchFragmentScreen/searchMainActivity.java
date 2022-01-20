285952453f4b723e22472347af4e01782e27ea55
Arhamirfan <arham.irfan4@gmail.com>
2022-01-20 15:34:03 +0500

    searching activity

--- app/src/main/java/com/example/suiterentals/homeScreen/SearchFragmentScreen/searchMainActivity.java
+++ app/src/main/java/com/example/suiterentals/homeScreen/SearchFragmentScreen/searchMainActivity.java
@@ -51,9 +51,8 @@ public class searchMainActivity extends AppCompatActivity {
         //Toast.makeText(searchMainActivity.this, "got text: "+toSearchName, Toast.LENGTH_SHORT).show();
         pd.setTitle("Getting Data");
         pd.show();
-        //for type 1, type 2 is setting recycler view data in its function
-//        getSearchItems();
-        getbyprice(toSearchName);
+
+        getbytitle(toSearchName);
         adapter = new secondAdapter(productListtype2);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         myrecyclerview.setLayoutManager(layoutManager);
@@ -62,60 +61,11 @@ public class searchMainActivity extends AppCompatActivity {
         //TODO: watch searching on youtube..
     }
 
-    //type 1: (Error in loop getting no data found) so trying type 2
-    private void getSearchItems() {
-        productList.clear();
-        finalproductlist.clear();
-        db.collection("Products").get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        productList.clear();
-                        if (queryDocumentSnapshots.isEmpty()) {
-                            Toast.makeText(getApplicationContext(), "Empty List of Products.", Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            List<Product> types = queryDocumentSnapshots.toObjects(Product.class);
-                            productList.addAll(types);
-                            for(int i=0;i<productList.size();i++)
-                            {
-                                if(productList.get(i).getSuitetitle() == toSearchName)
-                                {
-                                    Toast.makeText(getApplicationContext(), "Found Data in Title", Toast.LENGTH_SHORT).show();
-                                    finalproductlist.add(productList.get(i));
-
-                                } else if(productList.get(i).getPrice() == toSearchName)
-                                {
-                                    Toast.makeText(getApplicationContext(), "Found Data in Price", Toast.LENGTH_SHORT).show();
-                                    finalproductlist.add(productList.get(i));
-
-                                }else if(productList.get(i).getRooms() == toSearchName)
-                                {
-                                    Toast.makeText(getApplicationContext(), "Found Data in Rooms", Toast.LENGTH_SHORT).show();
-                                    finalproductlist.add(productList.get(i));
-
-                                } else
-                                {
-                                    Toast.makeText(getApplicationContext(), "No data found ☻", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                            myrecyclerview.setAdapter(adapter);
-                            pd.cancel();
-
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(), "Error getting data!!!", Toast.LENGTH_LONG).show();
-            }
-        });
-    }
 
     //type 2: get by title
     private void getbytitle(String searchitem) {
         productListtype2.clear();
-        db.collection("users").whereEqualTo("suitetitle", searchitem)
+        db.collection("Products").whereEqualTo("suitetitle", searchitem)
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot snapshots, @Nullable FirebaseFirestoreException e) {
@@ -129,14 +79,20 @@ public class searchMainActivity extends AppCompatActivity {
                             Product product = doc.toObject(Product.class);
                             productListtype2.add(product);
                         }
-                        updateListUsers(productListtype2);
+                        if(productListtype2.isEmpty())
+                        {
+                            getbyprice(searchitem);
+                        }else {
+                            myrecyclerview.setAdapter(adapter);
+                            pd.cancel();
+                        }
                     }
                 });
     }
 
     private void getbyprice(String searchitem) {
         productListtype2.clear();
-        db.collection("users").whereEqualTo("price", searchitem)
+        db.collection("Products").whereEqualTo("price", searchitem)
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot snapshots, @Nullable FirebaseFirestoreException e) {
@@ -155,30 +111,5 @@ public class searchMainActivity extends AppCompatActivity {
                         //updateListUsers(productListtype2);
                     }
                 });
-    }
-
-    private void updateListUsers(ArrayList<Product> listUsers) {
-
-        // Sort the list by date
-        Collections.sort(listUsers, new Comparator<Product>() {
-            @Override
-            public int compare(Product o1, Product o2) {
-                int res = -1;
-                if (o1.getSuitetitle().length() > (o2.getSuitetitle().length())) {
-                    res = 1;
-                } else if(Integer.valueOf(o1.getPrice()) > (Integer.valueOf(o2.getPrice())) )
-                {
-                    res =1;
-                }
-                return res;
-            }
-        });
-
-//        adaptertype2 = new secondAdapter(listUsers);
-////        rvtype2.setNestedScrollingEnabled(false);
-//        rvtype2.setAdapter(adaptertype2);
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-//        rvtype2.setLayoutManager(layoutManager);
-//        adaptertype2.notifyDataSetChanged();
     }
 }
